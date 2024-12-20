a500d54b47305095f747a4603718f296f132dca7
Afzal Diwan <afzal@relso.today>
2024-12-20 11:36:14 +0530

    added delete note and update note controlller

--- src/controllers/note.ts
+++ src/controllers/note.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction, RequestHandler } from "express";
 import { prisma } from "../lib/prisma-client.js";
 import { UnauthorizedError } from "../errors/unauthorized.js";
 import { BadRequestError } from "../errors/bad-request.js";
+import { NotFoundError } from "../errors/not-found.js";
 
 const createNote: RequestHandler = async (
   req: Request,
@@ -84,4 +85,118 @@ const createNote: RequestHandler = async (
   }
 };
 
-export { createNote };
+const updateNote: RequestHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const userId = req?.user?.id;
+    const noteId = req?.params?.id;
+
+    const {
+      title,
+      content,
+      name,
+      tags,
+    }: Partial<{
+      title: string;
+      content: string;
+      name: string;
+      tags: Array<{ name: string }> | undefined;
+    }> = req?.body;
+
+    if (!userId) return next(new UnauthorizedError("User unauthorized"));
+    if (!noteId) return next(new BadRequestError("Note id is required"));
+
+    const existingNote = await prisma.note.findUnique({
+      where: { id: noteId },
+    });
+
+    if (!existingNote || existingNote.user_id !== userId)
+      return next(new NotFoundError("Note not found"));
+
+    const updatedNote = await prisma.$transaction(async (prisma) => {
+      const category = name
+        ? await prisma.category.upsert({
+            where: { name_user_id: { name, user_id: userId } },
+            update: {},
+            create: { name, user_id: userId },
+          })
+        : null;
+
+      const updatedNote = await prisma.note.update({
+        where: { id: noteId },
+        data: {
+          title: title || existingNote.title,
+          content: content || existingNote.content,
+          category_id: category?.id || existingNote.category_id,
+        },
+      });
+
+      if (tags && tags.length > 0) {
+        const uniqueTags = tags.map((tag) => ({
+          name: tag.name,
+          user_id: userId,
+        }));
+
+        await prisma.tag.createMany({
+          data: uniqueTags,
+          skipDuplicates: true,
+        });
+
+        const tagRecords = await prisma.tag.findMany({
+          where: { name: { in: tags.map((tag) => tag.name) }, user_id: userId },
+        });
+
+        await prisma.noteTag.deleteMany({
+          where: { note_id: noteId },
+        });
+
+        await prisma.noteTag.createMany({
+          data: tagRecords.map((tag) => ({ note_id: noteId, tag_id: tag.id })),
+        });
+      }
+
+      return updatedNote;
+    });
+
+    const jsonBody = {
+      error: false,
+      message: "Note updated successfully!",
+      note: updatedNote,
+    };
+
+    res.status(200).json(jsonBody);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteNote: RequestHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const noteId = req?.params?.id;
+    const userId = req?.user?.id;
+
+    if (!userId) return next(new UnauthorizedError("User unauthorized"));
+    if (!noteId) return next(new NotFoundError("Note not found"));
+
+    await prisma.note.delete({
+      where: { id: userId },
+    });
+
+    const jsonBody = {
+      error: false,
+      message: "Note deleted successfully!",
+    };
+
+    res.status(200).json(jsonBody);
+  } catch (error) {
+    next(error);
+  }
+};
+export { createNote, deleteNote, updateNote };
